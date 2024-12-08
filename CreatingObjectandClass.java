41534c646580b25f8ff67708c8ab5b23988a54f5
Dinesh C <100751188+DineshCokes@users.noreply.github.com>
2024-12-08 20:07:28 +0530

    First Code

---
+++ CreatingObjectandClass.java
@@ -0,0 +1,11 @@
+public class CreatingObjectandClass
+{
+    int id;
+    String a;
+    public static void main(String[] args) {
+        CreatingObjectandClass s1=new CreatingObjectandClass();
+        System.out.println(s1.a);
+        System.out.println(s1.id);
+        
+    }
+}
